f9f347083c8785db064263f7fe7ed273dfb7a616
ahmeetarif <ahmetarif1254@gmail.com>
2025-01-18 14:03:47 +0300

    updagte

--- DemoAutoUpdate/obj/Debug/net8.0/DemoAutoUpdate.AssemblyInfo.cs
+++ DemoAutoUpdate/obj/Debug/net8.0/DemoAutoUpdate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoAutoUpdate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2d4fec9d99832bf8c581777474fd6ebffa0a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoAutoUpdate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoAutoUpdate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
